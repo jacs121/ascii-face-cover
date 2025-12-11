371c626f78f77b86a97e80298957e0519e809017
jacs121 <nitzansoriano1@gmail.com>
2025-12-11 15:12:27 +0200

    fix: Ensure installation of PIL._tkinter_finder succeeds in build script

--- build.sh
+++ build.sh
@@ -3,14 +3,22 @@
 PLATFORM=$1
 
 # Install dependencies
-pip install pyinstaller opencv-python mediapipe pillow pyvirtualcam
-pip install PIL._tkinter_finder || true
+pip install --no-input pyinstaller opencv-python mediapipe pillow pyvirtualcam
+pip install --no-input PIL._tkinter_finder || true
 
 if [ "$PLATFORM" = "windows" ]; then
-    pyinstaller --noconfirm --onefile --console --name "ASCII face cover - windows" --collect-all "mediapipe" --hidden-import="PIL._tkinter_finder" --clean  "/home/nitzan/Documents/my shish/programming/python/ascii emoji face cover/main.py"
+    pyinstaller --noconfirm --onefile --console \
+        --name "ASCII-face-cover-windows" \
+        --collect-all "mediapipe" \
+        --hidden-import="PIL._tkinter_finder" \
+        --clean main.py
 
 elif [ "$PLATFORM" = "linux" ]; then
-    pyinstaller --noconfirm --onefile --console --name "ASCII face cover - linux" --collect-all "mediapipe" --hidden-import="PIL._tkinter_finder" --clean  "/home/nitzan/Documents/my shish/programming/python/ascii emoji face cover/main.py"
+    pyinstaller --noconfirm --onefile --console \
+        --name "ASCII-face-cover-linux" \
+        --collect-all "mediapipe" \
+        --hidden-import="PIL._tkinter_finder" \
+        --clean main.py
 
 else
     echo "Usage: build.sh [windows|linux]"
